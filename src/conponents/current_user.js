3a43ad9d90f4ce3b4b864519ad3b315d15e79f76
kariman12 <marika.nakano.1212@gmail.com>
2020-05-25 01:09:32 +0900

    add form function

--- src/conponents/current_user.js
+++ src/conponents/current_user.js
@@ -1,61 +1,25 @@
 import React, { Component } from 'react';
 
-class CurrentUser extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            todos: [],
-            name: ''
-        };
-    }
-
-    onInput = (e) => {
-        this.setState({
-            name: e.target.value
-        });
-    }
-
-    addTodo = () => {
-        const {todos, name} = this.state;
-        this.setState({
-            todos: [...todos, name]
-        });
-    }
-
-    removeTodo = (index) => {
-        const {todos, name} = this.state;
-        this.setState({
-            todos: [...todos.slice(0, index), ...todos.slice(index + 1)]
-        });
-    }
-
-    render() {
-        const { todos } = this.state;
-        
-        return (
-            <div className="container">
-                <div className="user">
+const CurrentUser = (props) => (
+    <div className="current-user">
+        <div className="container">
+            <div className="user">
                 <img src={`${process.env.PUBLIC_URL}/panda.jpg`} className="user-image" alt="userimage" />
                 <div className="user-name">
-                <select>
-                    <option value='a'>Aさん</option>
-                    <option value='b'>Bさん</option>
-                    <option value='c'>Cさん</option>
-                </select>
+                    <select>
+                        <option value='a'>Aさん</option>
+                        <option value='b'>Bさん</option>
+                        <option value='c'>Cさん</option>
+                    </select>
                 </div>
-                </div>
-                <ul className='clap-list'>
-                    <li>拍手できる：70</li>
-                    <li>拍手された：20</li>
-                </ul>
             </div>
+            <ul className='clap-list'>
+                <li>拍手できる：70</li>
+                <li>拍手された：20</li>
+            </ul>
+        </div>
+    </div>
+    );
 
-
-        );
-
-
-    }
-}
 
 export default CurrentUser;
--- src/conponents/post.js
+++ src/conponents/post.js
@@ -1,66 +1,34 @@
 import React, { Component } from 'react';
 
-class Post extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            todos: [],
-            name: ''
-        };
-    }
-
-    onInput = (e) => {
-        this.setState({
-            name: e.target.value
-        });
-    }
-
-    addTodo = () => {
-        const {todos, name} = this.state;
-        this.setState({
-            todos: [...todos, name]
-        });
-    }
-
-    removeTodo = (index) => {
-        const {todos, name} = this.state;
-        this.setState({
-            todos: [...todos.slice(0, index), ...todos.slice(index + 1)]
-        });
-    }
-
-    render() {
-        const { todos } = this.state;
-        
-        return (
-            <div className = "container">
-                <div className="user">
+const Post = (props) => (
+    <div className="post">
+        <div className = "container">
+            <div className="user">
                 <img src={`${process.env.PUBLIC_URL}/neko.jpg`} className="user-image" alt="userimage" />
                 <div className="user-name">
-                <select>
-                    <option value='a'>Aさん</option>
-                    <option value='b'>Bさん</option>
-                    <option value='c'>Cさん</option>
-                </select>
-                </div>
-                </div>
-                <div className="post-text">
-                <textarea type="text" onInput={this.onInput} />
-                <button onClick={this.addTodo}>投稿</button>
-                <ul>
-                    {todos.map((todo, index) => <li key={index}>{todo}
-                    <button onClick={() => {this.removeTodo(index)}}>削除</button>
-                    </li>)}
-                </ul>
+                    <select>
+                        <option value='a'>Aさん</option>
+                        <option value='b'>Bさん</option>
+                        <option value='c'>Cさん</option>
+                    </select>
                 </div>
             </div>
+            <div className="post-text">
+            {/* <textarea type="text" onInput={props.handleAdd} /> */}
+            <form onSubmit={props.handleAdd}>
+            <textarea type="text" name="title"/>
+            {/* <button onClick={props.handleAdd}>投稿</button> */}
+            <input type="submit" value="投稿"/>
+            </form>
+            {/* <ul>
+                {todos.map((todo, index) => <li key={index}>{index + todo}
+                <button onClick={() => {this.removeTodo(index)}}>削除</button>
+                </li>)}
+            </ul> */}
+            </div>
+        </div>
+    </div>
+);
 
-
-        );
-
-
-    }
-}
 
 export default Post;
