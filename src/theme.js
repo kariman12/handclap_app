70230bad0b6be62e133c842a0f0c9201116ca840
kariman12 <marika.nakano.1212@gmail.com>
2020-05-30 22:39:45 +0900

    add css file

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import {createMuiTheme} from '@material-ui/core/styles'
+        
+export const theme = createMuiTheme({  // #1
+  palette: {
+    primary: {
+      light: '#9c786c',
+      main: '#6d4c41',
+      dark: '#40241a',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      light: '#ffa4a2',
+      main: '#e57373',
+      dark: '#af4448',
+      contrastText: '#ffffff',
+    },
+  },
+})
