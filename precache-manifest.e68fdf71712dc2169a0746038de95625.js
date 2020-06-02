c1f41b887bf39bf7657cd8d6e5f1f96d1c18450d
kariman12 <marika.nakano.1212@gmail.com>
2020-06-02 14:50:33 +0900

    Updates

---
+++ precache-manifest.e68fdf71712dc2169a0746038de95625.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135368e8bef341170128d869e6805ed5",
+    "url": "/handclap_app/index.html"
+  },
+  {
+    "revision": "3e575bdfd2ecc733f1f1",
+    "url": "/handclap_app/static/css/main.df94d366.chunk.css"
+  },
+  {
+    "revision": "1400c9f455638ab0847c",
+    "url": "/handclap_app/static/js/2.23f3be7d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/handclap_app/static/js/2.23f3be7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e575bdfd2ecc733f1f1",
+    "url": "/handclap_app/static/js/main.09c0f9aa.chunk.js"
+  },
+  {
+    "revision": "a081c57640f37851c323",
+    "url": "/handclap_app/static/js/runtime-main.e9c4cf20.js"
+  }
+]);
